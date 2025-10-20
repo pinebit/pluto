e0ee1f804e88307cda6c41be7c2e69acb05f4cb6
Lautaro Emanuel <31224949+emlautarom1@users.noreply.github.com>
2025-10-20 13:36:33 -0300

    feat: Add Nix support (#11)
    * Add Nix support
    
    * Add `typos`
    
    * Apply copilot suggestions
    
    * Update Cargo.lock
    
    * Rely on system libraries
    
    - Cannot set up nightly without breaking stuff
    
    * Use `git config` instead of `ln`

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/d7f52a7a640bc54c7bb414cca603835bf8dd4b10";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo-sort
+          cargo-deny
+
+          typos
+        ];
+
+        shellHook = ''
+          chmod +x .githooks/* && git config --local core.hooksPath .githooks/
+        '';
+      };
+
+    }
+  );
+}
